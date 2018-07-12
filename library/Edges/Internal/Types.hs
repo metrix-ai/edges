c8253f121266ad431a5f6c7ac9b94597c8c58b5b
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-07-12 22:00:19 +0300

    Ditch Count

--- library/Edges/Internal/Types.hs
+++ library/Edges/Internal/Types.hs
@@ -12,8 +12,3 @@ data Edges source target =
 newtype Node entity = Node Int
 
 newtype NodeCounts entity = NodeCounts (PrimArray Word32)
-
-data Count source target =
-  Count
-    {-# UNPACK #-} !Int {-^ Target count array size -}
-    (Int {-^ Index -} -> Word32 {-^ Count -} -> TVarArray Word32 {-^ Target count array -} -> IO ())
