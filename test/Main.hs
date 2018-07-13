f5b5c730ea77ee871698f3dc809db0e374949239
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-07-13 13:52:51 +0300

    Add construction tests

---
+++ test/Main.hs
@@ -0,0 +1,41 @@
+module Main where
+
+import Prelude
+import Test.QuickCheck.Instances
+import Test.Tasty
+import Test.Tasty.Runners
+import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
+import qualified Edges.Edges as A
+import qualified Edges.NodeCounts as B
+import qualified Edges.Node as C
+
+main =
+  defaultMain $
+  testGroup "All tests" $
+  [
+    testGroup "Bipartite" $ let
+      edgeList :: [(C.Node (Proxy 1), C.Node (Proxy 2))]
+      edgeList =
+        fmap (bimap C.Node C.Node) $
+        [
+          (0, 0),
+          (0, 1),
+          (0, 2),
+          (1, 0),
+          (1, 1),
+          (2, 0)
+        ]
+      (edges1, edges2) = A.listBipartite edgeList
+      in
+        [
+          testCase "Constructs the forward edges correctly" $ let
+            reconstructedEdgeList = A.toAssocList edges1
+            in assertEqual (show reconstructedEdgeList) edgeList reconstructedEdgeList
+          ,
+          testCase "Constructs the backward edges correctly" $ let
+            reconstructedEdgeList = sort $ A.toAssocList edges2
+            expectedEdgeList = sort $ fmap swap edgeList
+            in assertEqual (show reconstructedEdgeList) expectedEdgeList reconstructedEdgeList
+        ]
+  ]
